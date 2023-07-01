60c0df0a651f1402883e9453add6ce1a29969766
Malinga de Silva <rdimalingadesilva@gmail.com>
2023-07-01 15:21:40 +0530

    jwt_completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
 
-const Navbar = () => {
+const Navbar = async () => {
 
-const session = null;
+const session = await getCurrentUser();
 
   return (
     <nav className='flexBetween navbar'>
@@ -31,9 +32,16 @@ const session = null;
         </div>
 
         <div className='flexCenter gap-4'>
-            {session ? (
+            {session?.user ? (
                 <>
-                    UserPhoto 
+                    {session?.user?.image &&
+                        (<Image 
+                            src={session.user.image}
+                            width={40}
+                            height={40}
+                            className='rounded-full'
+                            alt={session.user.name}
+                        />)}
 
                     <Link href='/create-project'>Share your work</Link>
                 </>
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+//@ts-ignore
 const User = g.model('User', {
   name: g.string().length({min:2, max: 20},),
   email: g.string().unique(),
@@ -8,8 +9,11 @@ const User = g.model('User', {
   gitHubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
   projects: g.relation(() => Projects).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
 })
 
+//@ts-ignore
 const Projects = g.model('Projects', {
   title: g.string().length({min:3}),
   description: g.string(),
@@ -18,7 +22,20 @@ const Projects = g.model('Projects', {
   gitHubUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User)
+}).auth((rules) => {
+  rules.public().read(),
+  rules.private().create().delete().update();
 })
+
+const jwt = auth.JWT({
+  issuer: 'gratbase',
+  secret: g.env('NEXTAUTH_SECRET'),
+})
+
 export default config({
-  schema: g
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private(),
+  }
 })
